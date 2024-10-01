7477c224f7f6cdd6c5c06da6174c8070db72ff98
Andrey Podmarkov[A <abptv@mail.ru>
2024-10-01 11:02:30 +0400

    продолжаю писать бота угадайку

--- Guess the number.py
+++ Guess the number.py
@@ -53,3 +53,44 @@ async def stop(message: Message):
         user['in_game'] = False
         await message.answer(f'''Игра прервана''')
 
+@dp.message(F.text.lower().in_(['да', 'давай', 'сыграем', 'игра',
+                                'играть', 'хочу играть']))
+async def start_game(message: Message):
+    if not user['in_game']:
+        user['in_game'] = True
+        user['secret'] = secret_number()
+        user['games'] += 1
+        await message.answer(f'''Игра началась! Количество попыток: {user["attempts"]}''')
+    else:
+        await message.answer(f'''Игра уже идет. 
+Вы можете:
+Прислать число от 1 до 100
+Или ввести /stat для просмотра статистики
+или /cancel для прерывания игры
+''')
+
+@dp.message(F.text.lower().in_(['нет', 'не хочу', 'в другой раз']))
+async def not_game(message: Message):
+    if not user['in_game']:
+        await message.answer(f'''Жаль, как захотите играть, приходите''')
+    else:
+        await message.answer(f'''Мы же уже играем, пришлите число от 1 до 100''')
+
+@dp.message(F.text.isdigit())
+async def guess_number(message: Message):
+    if user['in_game']:
+        guess = int(message.text)
+        if guess in range(1, 101):
+            if guess == user['secret']:
+                user['won'] += 1
+                await message.answer(f'Вы угадали! Загаданное число: {user["secret"]}')
+            elif guess < user['secret']:
+                await message.answer(f'Вы ввели число меньше загаданного. Осталось попыток: {user["attempts"] - 1}')
+                user['attempts'] -= 1
+            else:
+                await message.answer(f'Вы ввели число больше загаданного. Осталось попыток: {user["attempts"] - 1}')
+                user['attempts'] -= 1
+        else:
+            await message.answer(f'Введите число от 1 до 100')
+
+
